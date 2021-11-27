a6c6605830ea14abc3837fa783ffeeecb8435161
heraya <herayafpm@gmail.com>
2021-11-27 11:31:55 +0700

    add multiple client

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Client;
+use Laravel\Passport\ClientRepository;
 
 class AuthController extends Controller
 {
@@ -13,11 +17,15 @@ class AuthController extends Controller
             'message' => '',
             'data' => []
         ];
+        App::clearResolvedInstance(ClientRepository::class);
+        app()->singleton(ClientRepository::class, function () {
+            return new ClientRepository(request()->header('clientId'), null); // You should give the client id in the first parameter
+        });
         if(Auth::attempt(['username' => request('username'),'password' => request('password')])){
             $user = Auth::user();
             $res['status'] = true;
             $res['message'] = "Login Berhasil";
-            $res['data']['token'] = "Bearer ".$user->createToken("web_".request('username'))->accessToken;
+            $res['data']['token'] = "Bearer ".$user->createToken("android_".request('username'))->accessToken;
             return response()->json($res,200);
         }
         $res['message'] = 'Unauthorized';
